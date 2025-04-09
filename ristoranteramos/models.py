97e2b0eee51afdf32a0c87bb83797463321db249
mariaartt <marteagaramos@safareyes.es>
2025-04-09 18:39:22 +0200

    realizo pruebas para el makemigrations

--- ristoranteramos/models.py
+++ ristoranteramos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Articulo(models.Model):
+    nombre =models.CharField(max_length=50)
+    descripcion =models.TextField()
+
+    def __str__(self):
+        return self.nombre
