d432d13eb734f7d7265b6a2ccb6845c6a82a1189
mariaartt <marteagaramos@safareyes.es>
2025-04-09 17:34:59 +0200

    actualizo librerias

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
+    'django_bootstrap_icons',
+    'ristoranteramos',
 ]
 
 MIDDLEWARE = [
