4d4ef0a09d4293887836d37b40ac757a17a8a74c
mariaartt <marteagaramos@safareyes.es>
2025-04-09 18:07:40 +0200

    commit con cambios de ivan de models.py

--- ristoranteramos/models.py
+++ ristoranteramos/models.py
@@ -23,3 +23,38 @@ class Articulo(models.Model):
         return self.nombre
 
 
+
+class Empleado(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=150)
+    rol = models.CharField(
+        max_length=50,
+        choices = Rol.choices,
+        default = empty
+    )
+
+    def __str__(self):
+        return self.nombre
+
+# Pedido hecho por cliente o camarero
+class Pedido(models.Model):
+    id = models.AutoField(primary_key=True)
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Pedido {self.id}"
+
+    def total(self):
+        return sum(linea.subtotal() for linea in self.lineapedido_set.all())
+
+# Tabla intermedia para asociar artículos a un pedido con cantidad
+class LineaPedido(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    articulo = models.ForeignKey(Articulo, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField(default=1)
+
+    def subtotal(self):
+        return self.articulo.precio * self.cantidad
+
+    def __str__(self):
+        return f"{self.cantidad} x {self.articulo.nombre} en Pedido {self.pedido.id}"
