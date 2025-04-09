e396a1d6371cf6285e748f9944faa2aa107a8ca1
mariaartt <marteagaramos@safareyes.es>
2025-04-09 18:02:52 +0200

    commit de models.py

--- ristoranteramos/models.py
+++ ristoranteramos/models.py
@@ -1,4 +1,25 @@
+from random import choices
+
 from django.db import models
+from django.forms import IntegerField
+from django.utils.functional import empty
+
+
+class Rol(models.TextChoices):
+    CAMARERO = 'CAMARERO', 'Camarero'
+    COCINERO = 'COCINERO', 'Cocinero'
+    ADMINISTRADOR = 'ADMINISTRADOR', 'Administrador'
+
 
 # Create your models here.
+class Articulo(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=150)
+    descripcion = models.TextField()
+    precio = IntegerField()
+    categoria = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
 
