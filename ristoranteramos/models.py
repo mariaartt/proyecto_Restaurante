7f99dc1a6f4fa25d07e8d5299f2b3f9a623ba7a1
mariaartt <marteagaramos@safareyes.es>
2025-04-30 20:52:03 +0200

    trabajo maria

--- ristoranteramos/models.py
+++ ristoranteramos/models.py
@@ -2,7 +2,6 @@ from random import choices
 
 from django.db import models
 from django.forms import IntegerField
-from django.utils.functional import empty
 
 
 class Rol(models.TextChoices):
@@ -13,44 +12,37 @@ class Rol(models.TextChoices):
 
 # Create your models here.
 class Articulo(models.Model):
-    id = models.AutoField(primary_key=True)
-
-
     nombre = models.CharField(max_length=150)
     descripcion = models.TextField()
     precio = IntegerField()
     categoria = models.CharField(max_length=100)
+    receta = models.TextField()
+    foto = models.ImageField()
+    tiempo_preparacion = models.IntegerField()
 
     def __str__(self):
         return self.nombre
 
-class Empleado(models.Model):
-    id = models.AutoField(primary_key=True)
 
-#
 class Empleado(models.Model):
-
     nombre = models.CharField(max_length=150)
     rol = models.CharField(
         max_length=50,
         choices = Rol.choices,
-
         default = ''
     )
 
     def __str__(self):
         return self.nombre
 
+
+
 # Pedido hecho por cliente o camarero
 class Pedido(models.Model):
-
     fecha = models.DateTimeField(auto_now_add=True)
-
+    articulo = models.ForeignKey(Articulo, on_delete=models.CASCADE)
     def __str__(self):
         return f"Pedido {self.id}"
-
-    # def total(self):
-    #     return sum(linea.subtotal() for linea in self.lineapedido_set.all())
 
 
 # Tabla intermedia para asociar artículos a un pedido con cantidad
@@ -61,6 +53,7 @@ class LineaPedido(models.Model):
 
     def subtotal(self):
         return self.articulo.precio * self.cantidad
+
 
     def __str__(self):
         return f"{self.cantidad} x {self.articulo.nombre} en Pedido {self.pedido.id}"
