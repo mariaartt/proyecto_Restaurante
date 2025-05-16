68a4fa3d547a87016c6b61b4760d829d00114623
ivanramoss-safa <iramosvillegas@safareyes.es>
2025-05-16 16:04:20 +0200

    arreglado db_config

--- restaurante/db_config.py
+++ restaurante/db_config.py
@@ -3,11 +3,11 @@ import cx_Oracle
 password = "1234"
 
 try:
-    # Intentar con el usuario C##RAMOS
-    cx_Oracle.connect("C##RAMOS", password, "localhost/XE")
-    db_user = "C##RAMOS"
-except cx_Oracle.DatabaseError:
-    # Si falla, usar C##MARIA
+    # Intentar con el usuario C##MARIA
+    cx_Oracle.connect("C##MARIA", password, "localhost/XE")
     db_user = "C##MARIA"
+except cx_Oracle.DatabaseError:
+    # Si falla, usar C##RAMOS
+    db_user = "C##RAMOS"
 
 db_password = password
